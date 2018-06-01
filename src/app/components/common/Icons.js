61ae0187d87af46d82981666a2644b1743e1b4db
chris-poe <chris.poe92@gmail.com>
2018-06-01 12:54:18 -0400

    Update Icon component

--- src/app/components/common/Icons.js
+++ src/app/components/common/Icons.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import * as FontAwesome from 'react-icons/lib/fa';
 
-const Icon = ({ name, style }) => {
+const Icons = ({ name, style }) => {
   const Icon = FontAwesome[name];
   return <Icon className={style} />;
 };
 
-export default Icon;
+export default Icons;
